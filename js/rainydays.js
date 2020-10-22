ccd5dd7a15d5df3689cada86815a62c7ddf28160
AndreaSletta <70203798+AndreaSletta@users.noreply.github.com>
2020-10-22 13:57:43 +0200

    +- button on cart page

--- js/rainydays.js
+++ js/rainydays.js
@@ -1,15 +1,27 @@
-function ScrollTo(name) {
-    ScrollToResolver(document.getElementById(name));
-  }
+// smooth scroll
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
   
-  function ScrollToResolver(elem) {
-    var jump = parseInt(elem.getBoundingClientRect().top * .2);
-    document.body.scrollTop += jump;
-    document.documentElement.scrollTop += jump;
-    if (!elem.lastjump || elem.lastjump > Math.abs(jump)) {
-      elem.lastjump = Math.abs(jump);
-      setTimeout(function() { ScrollToResolver(elem);}, "100");
-    } else {
-      elem.lastjump = null;
-    }
-  }
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
+  
